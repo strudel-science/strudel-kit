0c2298032a415437bf9bcf20bad91cb4fb526636
Cody O'Donnell <ctodonnell@lbl.gov>
2023-07-14 18:49:26 -0700

    Set up initial task flow and context provider

---
+++ src/task-flows/ExploringEntities.tsx
@@ -0,0 +1,70 @@
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import { DataGrid, GridColDef, GridToolbar, GridValueGetterParams } from '@mui/x-data-grid';
+import React from 'react';
+import { AnalyticsProvider } from '../components/AnalyticsProvider';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+  
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+  
+export const ExploringEntities: React.FC = () => {
+  return (
+    <AnalyticsProvider data={rows}>
+      <Box>
+        <Box component="nav">
+          <Typography variant="h5" component="h1">Project name</Typography>
+        </Box>
+        <Grid>
+            <Box>
+              <Typography>Entity List</Typography>
+              <Button>Filters</Button>
+              <TextField variant="outlined" label="Search" size="small"/>
+            </Box>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              disableColumnSelector
+              disableRowSelectionOnClick
+            />
+        </Grid>
+      </Box>
+    </AnalyticsProvider>
+  )
+}
