293b6e60f12a508339ca7f2ee17f01003bdaf80f
Cody O'Donnell <ctodonnell@lbl.gov>
2023-09-20 08:55:30 -0700

    Begin data list card layout

--- src/task-flows/ExploringDatasets/DataListPanel.tsx
+++ src/task-flows/ExploringDatasets/DataListPanel.tsx
@@ -45,22 +45,45 @@ export const DataListPanel: React.FC<EEDataPanelProps> = (props) => {
         >
           Sort
         </Button>
-        <TextField 
-          variant="outlined"
-          label="Search" 
-          size="small"
-          onChange={handleSearch}
-        />
+        <Box flex={1}>
+          <TextField 
+            variant="outlined"
+            label="Search" 
+            size="small"
+            fullWidth
+            onChange={handleSearch}
+          />
+        </Box>
       </Stack>
-      <Stack>
-        {state.data?.map((item, i) => (
-          <Stack direction="row">
-            <Box>Image</Box>
-            <Box>
-              <Typography>{item.title}</Typography>
-            </Box>
-          </Stack>
-        ))}
+      <Stack 
+        direction="row"
+        sx={{
+          padding: 2
+        }}
+      >
+        <Stack flex={1}>
+          {state.data?.map((item, i) => (
+            <Stack direction="row">
+              <Box sx={{ bgcolor: "neutral.main", textAlign: "center", height: 50, width: 50 }}>Image</Box>
+              <Box flex={1}>
+                <Typography sx={{ color: 'primary.main', fontWeight: 'bold' }}>{item.title}</Typography>
+                <Typography>{item.summary}</Typography>
+                <Stack direction="row" spacing={0} sx={{ fontStyle: 'italic' }}>
+                  <Typography sx={{ fontSize: 'small', fontWeight: 'bold', marginRight: 0.5 }}>Tags:</Typography>
+                  {item.tags.map((tag: string, i: number) => {
+                    if (i < item.tags.length -1) {
+                      return <Typography sx={{ fontSize: 'small', marginRight: 0.5 }}>{tag},</Typography>
+                    } else {
+                      return <Typography sx={{ fontSize: 'small' }}>{tag}</Typography>
+                    }
+                  })}
+                </Stack>
+                
+              </Box>
+            </Stack>
+          ))}
+        </Stack>
+        <Box sx={{ bgcolor: "neutral.main", textAlign: "center", height: 700, width: 400 }}>Map</Box>
       </Stack>
       <Pagination count={10} />
     </Paper>      
