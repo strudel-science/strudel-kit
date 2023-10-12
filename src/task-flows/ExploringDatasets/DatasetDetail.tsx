35ab0b9d6352027bd66389bf1cde42e8ba089626
Cody O'Donnell <ctodonnell@lbl.gov>
2023-10-12 16:45:11 -0700

    Add responsive container to dataset detail page

--- src/task-flows/ExploringDatasets/DatasetDetail.tsx
+++ src/task-flows/ExploringDatasets/DatasetDetail.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Link, Grid, IconButton, Paper, Stack, TextField, Toolbar, Typography, Tab, Button } from '@mui/material';
+import { AppBar, Box, Link, Grid, IconButton, Paper, Stack, TextField, Toolbar, Typography, Tab, Button, Container } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import CircleIcon from '@mui/icons-material/Circle';
@@ -68,140 +68,142 @@ export const DatasetDetail: React.FC = () => {
           backgroundColor: 'white'
         }}
       />
-      <Grid container spacing={1} sx={{ pt: 1, pr: 2, pb: 2, pl: 2,  }}>
-        {dataset && (
-          <>
-            <Grid item xs={8}>
-              <Paper sx={{ mb: 1 }}>
-                <Stack p={2}>
-                  <Stack direction="row">
+      <Container maxWidth="xl">
+        <Grid container spacing={1} sx={{ pt: 1, pr: 2, pb: 2, pl: 2,  }}>
+          {dataset && (
+            <>
+              <Grid item md={8} xs={12}>
+                <Paper sx={{ mb: 1 }}>
+                  <Stack p={2}>
+                    <Stack direction="row">
+                      <Box>
+                        <Typography variant="h6" component="h2" mb={1}>Dates</Typography>
+                        <LabelValueTable
+                          rows={[
+                            { label: 'Publication', value: dataset['publication_date'] },
+                            { label: 'Start Date', value: dataset['start_date'] },
+                            { label: 'End Date', value: dataset['end_date'] }
+                          ]}
+                        />
+                      </Box>
+                      <Box flex={1}>
+                        <Typography variant="h6" component="h2" mb={1}>Citation</Typography>
+                        <Typography>{dataset['citation']}</Typography>
+                      </Box>
+                    </Stack>
                     <Box>
-                      <Typography variant="h6" component="h2" mb={1}>Dates</Typography>
+                      <Typography variant="h6" component="h2" mb={1}>Summary</Typography>
+                      <Typography>{dataset['summary']}</Typography>
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" component="h2" mb={1}>Purpose</Typography>
+                      <Typography>{dataset['purpose']}</Typography>
+                    </Box>
+                  </Stack>
+                </Paper>
+                <Paper>
+                  <TabContext value={dataTabsValue}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                      <TabList onChange={handleDataTabsChange} aria-label="lab API tabs example">
+                        <Tab label="Data Files" value="1" />
+                        <Tab label="Data Types" value="2" />
+                        <Tab label="Metadata" value="3" />
+                      </TabList>
+                    </Box>
+                    <TabPanel value="1">
+                      <Stack direction="row" justifyContent="flex-end">
+                          <Button variant="contained">Download all files</Button>
+                      </Stack>
+                      <DataGrid
+                        rows={dataset['attached_files']}
+                        getRowId={(row) => row.file_id}
+                        columns={attachedFilesColumns}
+                        disableColumnSelector
+                        disableRowSelectionOnClick
+                        sx={{
+                          border: 'none'
+                        }}
+                      />
+                    </TabPanel>
+                    <TabPanel value="2">Tab Two Content</TabPanel>
+                    <TabPanel value="3">Tab Three Content</TabPanel>
+                  </TabContext>
+                </Paper>
+              </Grid>
+              <Grid item md={4} xs={12}>
+                <Paper>
+                  <Stack p={2}>
+                    <Box>
+                      <Typography variant="h6" component="h2" mb={1}>Map</Typography>
+                      <Box 
+                        sx={{ 
+                          display: 'flex', 
+                          alignItems: 'center', 
+                          justifyContent: "center", 
+                          bgcolor: "neutral.dark", 
+                          height: 300 
+                        }}
+                      >
+                        <Typography>{'<Map>'}</Typography>
+                      </Box>
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" component="h2" mb={1}>Tags</Typography>
+                        {dataset.tags.map((tag: string, i: number) => {
+                          if (i < dataset.tags.length -1) {
+                            return <Typography component="span" sx={{ fontSize: 'small', marginRight: 0.5 }}>{tag},</Typography>
+                          } else {
+                            return <Typography component="span" sx={{ fontSize: 'small' }}>{tag}</Typography>
+                          }
+                        })}
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" component="h2" mb={1}>Communities</Typography>
+                      <ul>
+                        {dataset['communities'].map((community: any, i: number) => (
+                          <li>
+                            <Link href={community.url} target='_blank'>
+                              {community.title}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" component="h2" mb={1}>Associated Projects</Typography>
+                      <ul>
+                        {dataset['associated_projects'].map((project: any, i: number) => (
+                          <li>
+                            <Link href={project.url} target='_blank'>
+                              {project.title}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" component="h2" mb={1}>Contacts</Typography>
                       <LabelValueTable
                         rows={[
-                          { label: 'Publication', value: dataset['publication_date'] },
-                          { label: 'Start Date', value: dataset['start_date'] },
-                          { label: 'End Date', value: dataset['end_date'] }
+                          { label: 'Point of Contact', value: dataset['point_of_contact'] },
+                          { label: 'Originator', value: dataset['originator'] },
+                          { label: 'Metadata Contact', value: dataset['metadata_contact'] },
+                          { label: 'Publisher', value: dataset['publisher'] },
+                          { label: 'Distributor', value: dataset['distributor'] },
+                          { label: 'USGS Mission Area', value: dataset['usgs_mission_area'] }
                         ]}
                       />
                     </Box>
-                    <Box flex={1}>
-                      <Typography variant="h6" component="h2" mb={1}>Citation</Typography>
-                      <Typography>{dataset['citation']}</Typography>
-                    </Box>
                   </Stack>
-                  <Box>
-                    <Typography variant="h6" component="h2" mb={1}>Summary</Typography>
-                    <Typography>{dataset['summary']}</Typography>
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" component="h2" mb={1}>Purpose</Typography>
-                    <Typography>{dataset['purpose']}</Typography>
-                  </Box>
-                </Stack>
-              </Paper>
-              <Paper>
-                <TabContext value={dataTabsValue}>
-                  <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList onChange={handleDataTabsChange} aria-label="lab API tabs example">
-                      <Tab label="Data Files" value="1" />
-                      <Tab label="Data Types" value="2" />
-                      <Tab label="Metadata" value="3" />
-                    </TabList>
-                  </Box>
-                  <TabPanel value="1">
-                    <Stack direction="row" justifyContent="flex-end">
-                        <Button variant="contained">Download all files</Button>
-                    </Stack>
-                    <DataGrid
-                      rows={dataset['attached_files']}
-                      getRowId={(row) => row.file_id}
-                      columns={attachedFilesColumns}
-                      disableColumnSelector
-                      disableRowSelectionOnClick
-                      sx={{
-                        border: 'none'
-                      }}
-                    />
-                  </TabPanel>
-                  <TabPanel value="2">Tab Two Content</TabPanel>
-                  <TabPanel value="3">Tab Three Content</TabPanel>
-                </TabContext>
-              </Paper>
-            </Grid>
-            <Grid item xs={4}>
-              <Paper>
-                <Stack p={2}>
-                  <Box>
-                    <Typography variant="h6" component="h2" mb={1}>Map</Typography>
-                    <Box 
-                      sx={{ 
-                        display: 'flex', 
-                        alignItems: 'center', 
-                        justifyContent: "center", 
-                        bgcolor: "neutral.dark", 
-                        height: 300 
-                      }}
-                    >
-                      <Typography>{'<Map>'}</Typography>
-                    </Box>
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" component="h2" mb={1}>Tags</Typography>
-                      {dataset.tags.map((tag: string, i: number) => {
-                        if (i < dataset.tags.length -1) {
-                          return <Typography component="span" sx={{ fontSize: 'small', marginRight: 0.5 }}>{tag},</Typography>
-                        } else {
-                          return <Typography component="span" sx={{ fontSize: 'small' }}>{tag}</Typography>
-                        }
-                      })}
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" component="h2" mb={1}>Communities</Typography>
-                    <ul>
-                      {dataset['communities'].map((community: any, i: number) => (
-                        <li>
-                          <Link href={community.url} target='_blank'>
-                            {community.title}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" component="h2" mb={1}>Associated Projects</Typography>
-                    <ul>
-                      {dataset['associated_projects'].map((project: any, i: number) => (
-                        <li>
-                          <Link href={project.url} target='_blank'>
-                            {project.title}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" component="h2" mb={1}>Contacts</Typography>
-                    <LabelValueTable
-                      rows={[
-                        { label: 'Point of Contact', value: dataset['point_of_contact'] },
-                        { label: 'Originator', value: dataset['originator'] },
-                        { label: 'Metadata Contact', value: dataset['metadata_contact'] },
-                        { label: 'Publisher', value: dataset['publisher'] },
-                        { label: 'Distributor', value: dataset['distributor'] },
-                        { label: 'USGS Mission Area', value: dataset['usgs_mission_area'] }
-                      ]}
-                    />
-                  </Box>
-                </Stack>
-              </Paper>
-            </Grid>
-          </>
-        )}
-        {!dataset && (
-          <Typography>Could not find this dataset</Typography>
-        )}
-      </Grid>
+                </Paper>
+              </Grid>
+            </>
+          )}
+          {!dataset && (
+            <Typography>Could not find this dataset</Typography>
+          )}
+        </Grid>
+      </Container>
     </Box>
   )
 }
