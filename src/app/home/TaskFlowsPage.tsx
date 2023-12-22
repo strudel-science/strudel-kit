9b6cf35b841110bb72d4070b7e1eace36f1a1388
Cody O'Donnell <ctodonnell@lbl.gov>
2023-12-22 16:09:44 -0500

    Clean up home page

--- src/app/home/TaskFlowsPage.tsx
+++ src/app/home/TaskFlowsPage.tsx
@@ -1,33 +1,90 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { PropsWithChildren, ReactNode, useEffect, useState } from 'react';
 import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import * as d3 from 'd3-fetch';
-import { Stack, Link } from '@mui/material';
+import { Stack, Link, Container, Paper, List, ListItem, Typography, Box } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 export const TaskFlowsPage: React.FC = () => {
   return (
-    <Stack p={2}>
-      <Link component={RouterLink} to="exploring-datasets">
-        Exploring Datasets
-      </Link>
-      <Link component={RouterLink} to="exploring-entities">
-        Exploring Entities
-      </Link>
-      <Link component={RouterLink} to="optimization">
-        Optimization
-      </Link>
-      <Link component={RouterLink} to="contributing-data">
-        Contributing Data
-      </Link>
-      <Link component={RouterLink} to="monitor-tasks">
-        Monitor Tasks
-      </Link>
-      <Link component={RouterLink} to="compare-scenarios">
-        Compare Scenarios
-      </Link>
-      <Link component={RouterLink} to="myanalysis">
-        My Analysis
-      </Link>
-    </Stack>
+    <Container
+      maxWidth="md"
+      sx={{
+        marginTop: 3,
+        marginBottom: 3,
+      }}
+    >
+      <Paper
+        sx={{
+          padding: 2
+        }}
+      >
+        <Box
+          component="aside"
+          sx={{ 
+            position: 'relative', 
+            width: '250px' 
+          }}
+        >
+          <List>
+            <NavListItem>
+              <Link component={RouterLink} to="exploring-datasets">
+                Exploring Datasets
+              </Link>
+            </NavListItem>
+            <NavListItem>
+              <Link component={RouterLink} to="exploring-entities">
+                Exploring Entities
+              </Link>
+            </NavListItem>
+            <NavListItem>
+              <Link component={RouterLink} to="optimization">
+                Optimization
+              </Link> 
+            </NavListItem>
+            <NavListItem>
+              <Link component={RouterLink} to="contributing-data">
+                Contributing Data
+              </Link>
+            </NavListItem>
+            <NavListItem>
+              <Link component={RouterLink} to="monitor-tasks">
+                Monitor Tasks
+              </Link> 
+            </NavListItem>
+            <NavListItem>
+              <Link component={RouterLink} to="compare-scenarios">
+                Compare Scenarios
+              </Link>
+            </NavListItem>
+            <NavListItem>
+              <Link component={RouterLink} to="myanalysis">
+                My Analysis
+              </Link>
+            </NavListItem>
+          </List>
+        </Box>
+      </Paper>
+    </Container>
+  )
+}
+
+const NavListItem: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <ListItem
+      sx={{
+        padding: 0,
+      }}
+    >
+      <Typography
+        sx={{
+          padding: '0.5rem 1rem',
+          color: 'neutral.main',
+          fontSize: '1.125rem',
+          fontWeight: 'bold',
+        }}
+      >
+        {children}
+      </Typography>
+    </ListItem>
   )
 }
