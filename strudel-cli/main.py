23f7e6fbcbc30ca4b8728fef13569b8aab9f8e83
Cody O'Donnell <ctodonnell@lbl.gov>
2024-01-19 13:10:32 -0800

    Update cli commands to use gh repo and options

--- strudel-cli/main.py
+++ strudel-cli/main.py
@@ -7,13 +7,29 @@ app = typer.Typer()
 @app.command()
 def create_app():
   print("Creating your app...")
-  subprocess.run(["cookiecutter", "../strudel-cookiecutter/base", "--output-dir", "../strudel-cookiecutter/output"])
+  subprocess.run([
+    "cookiecutter", 
+    "gh:strudel-science/strudel-kit", 
+    "--checkout", 
+    "feature/cli",
+    "--directory", 
+    "strudel-cookiecutter/base"
+  ])
 
 
 @app.command()
-def add_taskflow(name: str):
+def add_taskflow(name: str, output_dir: str = "src/app"):
   print("Adding task flow to your app...")
-  subprocess.run(["cookiecutter", f"../strudel-cookiecutter/{name}", "--output-dir", "../strudel-cookiecutter/output/my_science_app/src/app"])
+  subprocess.run([
+    "cookiecutter", 
+    "gh:strudel-science/strudel-kit", 
+    "--checkout", 
+    "feature/cli",
+    "--directory", 
+    f"strudel-cookiecutter/{name}",
+    "--output-dir",
+    output_dir
+  ])
 
 
 if __name__ == "__main__":
