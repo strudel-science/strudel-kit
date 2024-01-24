990b7405fd25cd66b02eacbb28d5130de8bd10b2
Cody O'Donnell <ctodonnell@lbl.gov>
2024-01-24 15:59:00 -0800

    Start adding filters template option to explore data task flow

--- strudel-demo-app/src/app/explore-data/DataDetailPage.tsx
+++ strudel-demo-app/src/app/explore-data/DataDetailPage.tsx
@@ -1,11 +1,11 @@
 import { Box, Container } from '@mui/material';
 import React, { useState } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
-import { useAnalytics } from '../../components/contexts/analytics/AnalyticsProvider';
 import { PageHeader } from '../../components/PageHeader';
+import { useExploreData } from './context/ContextProvider';
   
 export const DataDetailPage: React.FC = () => {
-  const {state, dispatch} = useAnalytics();
+  const {state, dispatch} = useExploreData();
   const params = useParams();
 
   return (
