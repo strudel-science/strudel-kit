6da15694715829c2b9eb8e2f28a12742a3440787
Cody O'Donnell <ctodonnell@lbl.gov>
2024-02-06 16:06:52 -0800

    Fix typo

--- strudel-cookiecutter/explore-data/{{cookiecutter.name}}/columns.ts
+++ strudel-cookiecutter/explore-data/{{cookiecutter.name}}/columns.ts
@@ -1,7 +1,7 @@
 import { GridColDef } from "@mui/x-data-grid";
 
 export const columns: GridColDef[] = [
-  {% for column in cookiecutter.config.column %}
+  {% for column in cookiecutter.config.columns %}
     {{ column }}
   {% endfor %}
 ];
