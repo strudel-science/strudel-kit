217dd41237c4127ed7d52d2954b170520fdbecc3
Cody O'Donnell <ctodonnell@lbl.gov>
2024-04-17 15:58:33 -0700

    Use tagged version instead of branch for cookiecutter templates

--- strudel-cli/src/strudel/callbacks.py
+++ strudel-cli/src/strudel/callbacks.py
@@ -7,7 +7,7 @@ defaults = {
     "create-app": {
         "name": "",
         "output_dir": "",
-        "branch": "prod",
+        "branch": f"v{__version__}",
         "config": "",
         "verbose": 0
     },
@@ -15,7 +15,7 @@ defaults = {
         "name": "",
         "template": "",
         "output_dir": "src/app",
-        "branch": "prod",
+        "branch": f"v{__version__}",
         "config": "",
         "verbose": 0
     }
