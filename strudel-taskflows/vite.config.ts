2465eda12004e3bca3a038dc53e4809b1f79fe58
Cody O'Donnell <ctodonnell@lbl.gov>
2024-05-10 16:38:38 -0700

    Add gh-pages dependency and basePath

--- strudel-taskflows/vite.config.ts
+++ strudel-taskflows/vite.config.ts
@@ -5,4 +5,5 @@ import generouted from '@generouted/react-router/plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), generouted()],
+  base: '/strudel-kit/demo/'
 })
