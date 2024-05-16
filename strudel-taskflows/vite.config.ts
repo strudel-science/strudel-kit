7abe25a559e9fbae5163a43c3c5a54b85097b486
Cody O'Donnell <ctodonnell@lbl.gov>
2024-05-16 16:45:23 -0700

    Use base url in app build, routing, and requests

--- strudel-taskflows/vite.config.ts
+++ strudel-taskflows/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import generouted from '@generouted/react-router/plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), generouted()],
-  base: '/strudel-kit/demo/'
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), generouted()],
+    base: env.VITE_BASE_URL
+  }
 })
