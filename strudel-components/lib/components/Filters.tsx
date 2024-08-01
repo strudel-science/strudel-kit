ff31025edf6ff32f9546c9906925269267bcf5d3
Cody O'Donnell <ctodonnell@lbl.gov>
2024-08-01 16:59:40 -0700

    Add active label when no filter groups

--- strudel-components/lib/components/Filters.tsx
+++ strudel-components/lib/components/Filters.tsx
@@ -1,7 +1,8 @@
 import CloseIcon from '@mui/icons-material/Close';
-import { Box, Button, IconButton, Paper, PaperProps, Stack } from '@mui/material';
-import React, { useState } from 'react';
+import { Box, Button, Chip, IconButton, Paper, PaperProps, Stack } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { FilterContext, FilterState } from './FilterContext';
+import { hasValue } from './FilterField';
 
 
 interface FilterPanelProps extends PaperProps {
@@ -21,14 +22,38 @@ export const Filters: React.FC<FilterPanelProps> = ({
   children,
   ...rest
 }) => { 
-  const [activeFilters, setActiveFilters] = useState({});
+  const [activeFilters, setActiveFilters] = useState<any>({});
+
+  /**
+   * Count the number of active filters in this group by using
+   * the `field` prop from the FilterField children to look up
+   * that filter in `activeFilters` 
+   */
+  let activeChildren = 0;
+  React.Children.forEach(children, (child) => {
+    if (
+      React.isValidElement(child) &&
+      child.props.field &&
+      hasValue(activeFilters[child.props.field])
+    ) {
+      return activeChildren++
+    }
+  })
+
+  const handleChange = (filters: FilterState['activeFilters']) => {
+    setActiveFilters(filters);
+  }
 
   const handleReset = () => {
     setActiveFilters({});
   }
 
+  useEffect(() => {
+    if (onChange) onChange(activeFilters);
+  }, [activeFilters])
+
   return (
-    <FilterContext activeFilters={activeFilters} onChange={onChange}>
+    <FilterContext activeFilters={activeFilters} onChange={handleChange}>
       <Paper 
         elevation={0}
         variant="outlined"
@@ -48,7 +73,16 @@ export const Filters: React.FC<FilterPanelProps> = ({
                 paddingBottom: 1,
               }}
             >
-              <Box flex={1}>{header}</Box>
+              <Stack direction="row" spacing={2} flex={1}>
+                <Box>{header}</Box>
+                {activeChildren > 0 && (
+                  <Chip 
+                    label={`${activeChildren} active`} 
+                    color="primary" 
+                    size="small" 
+                  />
+                )}
+              </Stack>
               <Button 
                 variant="outlined" 
                 onClick={handleReset}
