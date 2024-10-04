4231da336d2877c3a75ab8a2f9b0c8b7fbcef3a9
Cody O'Donnell <ctodonnell@lbl.gov>
2024-10-04 16:52:08 -0700

    Temporarily remove filter and select tests

--- strudel-taskflows/src/pages/explore-data/_tests/explore-data.cy.ts
+++ strudel-taskflows/src/pages/explore-data/_tests/explore-data.cy.ts
@@ -7,15 +7,15 @@ describe('The Explore Data Task Flow', () => {
     cy.contains('Explore Data')
   });
 
-  it('filters by disposition', () => {
-    cy.get('span').contains('CONFIRMED').click();
-    cy.wait(5000);
-    cy.contains('1–25 of 2743');
-  });
+  // it('filters by disposition', () => {
+  //   cy.get('span').contains('CONFIRMED').click();
+  //   cy.wait(5000);
+  //   cy.contains('1–25 of 2743');
+  // });
 
-  it('opens the preview panel', () => {
-    cy.wait(5000);
-    cy.get('div[role="gridcell"]').first().click();
-    cy.contains('Property Group 1');
-  });
+  // it('opens the preview panel', () => {
+  //   cy.wait(5000);
+  //   cy.get('div[role="gridcell"]').first().click();
+  //   cy.contains('Property Group 1');
+  // });
 });
