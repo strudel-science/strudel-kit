537c74effc1a08052edd469796205e4a9d3032c2
Cody O'Donnell <ctodonnell@lbl.gov>
2024-10-07 08:02:19 -0700

    Fix path in fetchData when there's no env

--- strudel-taskflows/src/utils/queryParams.utils.ts
+++ strudel-taskflows/src/utils/queryParams.utils.ts
@@ -124,7 +124,8 @@ export const fetchData = async (dataSource: string) => {
   const base = document.querySelector('base')?.getAttribute('href') ?? '';
   // Use the VITE_BASE_URL env variable to specify a path prefix that 
   // should be added to routes and local requests
-  const basename = base + import.meta.env.VITE_BASE_URL;
+  const basePath = import.meta.env.VITE_BASE_URL || '';
+  const basename = base + basePath;
   const fileExtension = dataSource.split('.').pop();
   const isExternal = dataSource.startsWith('http');
   const dataSourcePath = isExternal ? cleanUrl(dataSource) : cleanUrl(`${basename}/${dataSource}`);
