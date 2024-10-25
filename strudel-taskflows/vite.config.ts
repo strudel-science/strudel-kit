889384bda8f65e582bcf90b7da7f46353208f6f2
Cody O'Donnell <ctodonnell@lbl.gov>
2024-10-25 10:20:55 -0700

    Add lint-staged for only linting staged files

--- strudel-taskflows/vite.config.ts
+++ strudel-taskflows/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import generouted from '@generouted/react-router/plugin';
 
-// Test
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	// Load env file based on `mode` in the current working directory.
