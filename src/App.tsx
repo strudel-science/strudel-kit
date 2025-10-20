bd252c5430fb49533f56f2de84d8d72c0075fd7c
Cody O'Donnell <ctodonnell@lbl.gov>
2025-10-20 11:22:37 -0700

    Convert router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { ThemeProvider } from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import {
+  createHashHistory,
+  createRouter,
+  RouterProvider,
+} from '@tanstack/react-router';
 import React from 'react';
 import { ApiModal } from './components/ApiModal';
 import { AppProvider } from './context/ContextProvider';
 import { routeTree } from './routeTree.gen';
 import { theme } from './theme';
 
-export const router = createRouter({ routeTree });
+const hashHistory = createHashHistory();
+export const router = createRouter({ routeTree, history: hashHistory });
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
